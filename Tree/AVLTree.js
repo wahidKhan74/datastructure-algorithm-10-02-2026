4964488a6cd385ffefa1b71676a2fb7d83ee5723
Waheed Khan <wahidXX@xxx.com>
2026-02-12 06:24:27 +0300

    Added AVLTree

---
+++ Tree/AVLTree.js
@@ -0,0 +1,3 @@
+// AVL Tree : An implementation of an AVL Tree, a self-balancing binary search tree.
+// where the height of the two child subtrees of any node differs by at most one.
+// This implementation includes methods for adding nodes, removing nodes, finding nodes, and traversing the tree.
